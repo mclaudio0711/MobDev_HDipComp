40713b479f533891d05fc804250b7d8431dbbd79
MarceloClaudio <mclaudio.ccyber@hotmail.com>
2022-05-16 21:27:04 +0000

    characters done

--- mobdev_ca3/src/app/pages/characters/characters.page.ts
+++ mobdev_ca3/src/app/pages/characters/characters.page.ts
@@ -11,7 +11,7 @@ import { ApiService } from '../../services/api.service';
 export class CharactersPage implements OnInit {
 
   characters: Observable<any>;
-  characterId: Observable<any>
+  characterId: Observable<any>;
 
   constructor(private router: Router, private api: ApiService) { }
 
