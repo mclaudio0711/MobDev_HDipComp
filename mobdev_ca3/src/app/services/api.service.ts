b9a3e162fe548979400598d1f2c0cb25745ea768
MarceloClaudio <mclaudio.ccyber@hotmail.com>
2022-05-18 14:18:23 +0000

    finishing touches

--- mobdev_ca3/src/app/services/api.service.ts
+++ mobdev_ca3/src/app/services/api.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
